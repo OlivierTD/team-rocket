934e28c612fd14a92d81fa20fb06a089ce539d32
SaiShan <saishan.ry@gmail.com>
2018-02-07 13:47:37 -0500

    app now openning on the good HomePage (personalized one) + the HomePageFragment is now properly working

--- app/src/main/java/de/danoeh/antennapod/fragment/HomePageFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/HomePageFragment.java
@@ -2,6 +2,11 @@ package de.danoeh.antennapod.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.activity.MainActivity;
 
 /**
  * Created by Sai Shan on 2018-02-02.
@@ -20,5 +25,23 @@ public class HomePageFragment extends Fragment{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+
+   @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View root = inflater.inflate(R.layout.home_page, container, false);
+        return root;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
     }
 }
