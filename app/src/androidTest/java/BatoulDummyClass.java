ddd01e670fb22be2c31206fcd3ffa8a38c71eeed
Batoul Yehia <batoul.yehia@gmail.com>
2018-02-11 14:39:51 -0500

    added dummy test file

---
+++ app/src/androidTest/java/BatoulDummyClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Batoul on 2018-02-09.
+ */
+
+import android.test.InstrumentationTestRunner;
+
+
+public class BatoulDummyClass extends InstrumentationTestRunner{
+
+    public void dummyTest(){
+        //testing the CI for pull request!
+        String name = "Batoul";
+        assert(name == "Batoul");
+
+    }
+
+}
