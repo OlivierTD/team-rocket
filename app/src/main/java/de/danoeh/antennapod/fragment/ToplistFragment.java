6d5304fcda86a80308252211443e8b11cc8b6864
davidlegare <davidlegare01@gmail.com>
2018-02-12 02:42:24 -0500

    itunes toplist on home page

---
+++ app/src/main/java/de/danoeh/antennapod/fragment/ToplistFragment.java
@@ -0,0 +1,228 @@
+package de.danoeh.antennapod.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.activity.OnlineFeedViewActivity;
+import de.danoeh.antennapod.adapter.itunes.ItunesAdapter;
+import de.danoeh.antennapod.core.ClientConfig;
+import de.danoeh.antennapod.core.service.download.AntennapodHttpClient;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by David on 2018-02-12.
+ */
+
+public class ToplistFragment extends Fragment{
+
+    private static final String TAG = "ItunesSearchFragment";
+
+    private static final String API_URL = "https://itunes.apple.com/search?media=podcast&term=%s";
+
+
+    /**
+     * Adapter responsible with the search results
+     */
+    private ItunesAdapter adapter;
+    private GridView gridView;
+    private ProgressBar progressBar;
+    private TextView txtvError;
+    private Button butRetry;
+    private TextView txtvEmpty;
+
+    /**
+     * List of podcasts retreived from the search
+     */
+    private List<ItunesAdapter.Podcast> searchResults;
+    private List<ItunesAdapter.Podcast> topList;
+    private Subscription subscription;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_itunes_search, container, false);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        adapter = new ItunesAdapter(getActivity(), new ArrayList<>());
+        gridView.setAdapter(adapter);
+
+        //Show information about the podcast when the list item is clicked
+        gridView.setOnItemClickListener((parent, view1, position, id) -> {
+            ItunesAdapter.Podcast podcast = searchResults.get(position);
+            if(podcast.feedUrl == null) {
+                return;
+            }
+            if (!podcast.feedUrl.contains("itunes.apple.com")) {
+                Intent intent = new Intent(getActivity(), OnlineFeedViewActivity.class);
+                intent.putExtra(OnlineFeedViewActivity.ARG_FEEDURL, podcast.feedUrl);
+                intent.putExtra(OnlineFeedViewActivity.ARG_TITLE, "iTunes");
+                startActivity(intent);
+            } else {
+                gridView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+                subscription = Observable.create((Observable.OnSubscribe<String>) subscriber -> {
+                    OkHttpClient client = AntennapodHttpClient.getHttpClient();
+                    Request.Builder httpReq = new Request.Builder()
+                            .url(podcast.feedUrl)
+                            .header("User-Agent", ClientConfig.USER_AGENT);
+                    try {
+                        Response response = client.newCall(httpReq.build()).execute();
+                        if (response.isSuccessful()) {
+                            String resultString = response.body().string();
+                            JSONObject result = new JSONObject(resultString);
+                            JSONObject results = result.getJSONArray("results").getJSONObject(0);
+                            String feedUrl = results.getString("feedUrl");
+                            subscriber.onNext(feedUrl);
+                        } else {
+                            String prefix = getString(R.string.error_msg_prefix);
+                            subscriber.onError(new IOException(prefix + response));
+                        }
+                    } catch (IOException | JSONException e) {
+                        subscriber.onError(e);
+                    }
+                    subscriber.onCompleted();
+                })
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(feedUrl -> {
+                            progressBar.setVisibility(View.GONE);
+                            gridView.setVisibility(View.VISIBLE);
+                            Intent intent = new Intent(getActivity(), OnlineFeedViewActivity.class);
+                            intent.putExtra(OnlineFeedViewActivity.ARG_FEEDURL, feedUrl);
+                            intent.putExtra(OnlineFeedViewActivity.ARG_TITLE, "iTunes");
+                            startActivity(intent);
+                        }, error -> {
+                            Log.e(TAG, Log.getStackTraceString(error));
+                            progressBar.setVisibility(View.GONE);
+                            gridView.setVisibility(View.VISIBLE);
+                            String prefix = getString(R.string.error_msg_prefix);
+                            new MaterialDialog.Builder(getActivity())
+                                    .content(prefix + " " + error.getMessage())
+                                    .neutralText(android.R.string.ok)
+                                    .show();
+                        });
+            }
+        });
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        txtvError = (TextView) view.findViewById(R.id.txtvError);
+        butRetry = (Button) view.findViewById(R.id.butRetry);
+        txtvEmpty = (TextView) view.findViewById(android.R.id.empty);
+
+        loadToplist();
+
+        return view;
+    }
+
+    private void loadToplist() {
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
+        gridView.setVisibility(View.GONE);
+        txtvError.setVisibility(View.GONE);
+        butRetry.setVisibility(View.GONE);
+        txtvEmpty.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        subscription = Observable.create((Observable.OnSubscribe<List<ItunesAdapter.Podcast>>) subscriber -> {
+            String lang = Locale.getDefault().getLanguage();
+            String url = "https://itunes.apple.com/" + lang + "/rss/toppodcasts/limit=25/explicit=true/json";
+            OkHttpClient client = AntennapodHttpClient.getHttpClient();
+            Request.Builder httpReq = new Request.Builder()
+                    .url(url)
+                    .header("User-Agent", ClientConfig.USER_AGENT);
+            List<ItunesAdapter.Podcast> results = new ArrayList<>();
+            try {
+                Response response = client.newCall(httpReq.build()).execute();
+                if(!response.isSuccessful()) {
+                    // toplist for language does not exist, fall back to united states
+                    url = "https://itunes.apple.com/us/rss/toppodcasts/limit=25/explicit=true/json";
+                    httpReq = new Request.Builder()
+                            .url(url)
+                            .header("User-Agent", ClientConfig.USER_AGENT);
+                    response = client.newCall(httpReq.build()).execute();
+                }
+                if(response.isSuccessful()) {
+                    String resultString = response.body().string();
+                    JSONObject result = new JSONObject(resultString);
+                    JSONObject feed = result.getJSONObject("feed");
+                    JSONArray entries = feed.getJSONArray("entry");
+
+                    for(int i=0; i < entries.length(); i++) {
+                        JSONObject json = entries.getJSONObject(i);
+                        ItunesAdapter.Podcast podcast = ItunesAdapter.Podcast.fromToplist(json);
+                        results.add(podcast);
+                    }
+                }
+                else {
+                    String prefix = getString(R.string.error_msg_prefix);
+                    subscriber.onError(new IOException(prefix + response));
+                }
+            } catch (IOException | JSONException e) {
+                subscriber.onError(e);
+            }
+            subscriber.onNext(results);
+            subscriber.onCompleted();
+        })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(podcasts -> {
+                    progressBar.setVisibility(View.GONE);
+                    topList = podcasts;
+                    updateData(topList);
+                }, error -> {
+                    Log.e(TAG, Log.getStackTraceString(error));
+                    progressBar.setVisibility(View.GONE);
+                    txtvError.setText(error.toString());
+                    txtvError.setVisibility(View.VISIBLE);
+                    butRetry.setOnClickListener(v -> loadToplist());
+                    butRetry.setVisibility(View.VISIBLE);
+                });
+    }
+
+    void updateData(List<ItunesAdapter.Podcast> result) {
+        this.searchResults = result;
+        adapter.clear();
+        if (result != null && result.size() > 0) {
+            gridView.setVisibility(View.VISIBLE);
+            txtvEmpty.setVisibility(View.GONE);
+            for (ItunesAdapter.Podcast p : result) {
+                adapter.add(p);
+            }
+            adapter.notifyDataSetInvalidated();
+        } else {
+            gridView.setVisibility(View.GONE);
+            txtvEmpty.setVisibility(View.VISIBLE);
+        }
+    }
+}
