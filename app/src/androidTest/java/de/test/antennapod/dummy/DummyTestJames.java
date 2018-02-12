ba0cc42bbfc085f5175f6b65fc3f4105260aefbe
DESKTOP-9KJ158Q\James <23jamesff@gmail.com>
2018-02-12 18:10:44 -0500

    adding dummy test

---
+++ app/src/androidTest/java/de/test/antennapod/dummy/DummyTestJames.java
@@ -0,0 +1,15 @@
+package de.test.antennapod.dummy;
+
+/**
+ * Created by James on 2018-02-12.
+ */
+
+import android.test.InstrumentationTestCase;
+
+
+public class DummyTestJames extends InstrumentationTestCase{
+
+    public void testJames(){
+        assertTrue(true);
+    }
+}
