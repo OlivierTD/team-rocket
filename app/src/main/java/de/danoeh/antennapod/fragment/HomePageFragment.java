aa02ca39a67d98e97fc9a131e8282fc64d2c285f
davidlegare <davidlegare01@gmail.com>
2018-03-01 22:35:03 -0500

    Added search bar to home page (only searches iTunes) #7

--- app/src/main/java/de/danoeh/antennapod/fragment/HomePageFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/HomePageFragment.java
@@ -1,13 +1,43 @@
 package de.danoeh.antennapod.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.MainActivity;
+import de.danoeh.antennapod.adapter.itunes.ItunesAdapter;
+import de.danoeh.antennapod.core.ClientConfig;
+import de.danoeh.antennapod.core.service.download.AntennapodHttpClient;
+import de.danoeh.antennapod.menuhandler.MenuItemUtils;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Sai Shan on 2018-02-02.
@@ -16,7 +46,20 @@ import de.danoeh.antennapod.activity.MainActivity;
 public class HomePageFragment extends Fragment{
 
     private TextView txtHome;
+
+    private ItunesAdapter adapter;
+    private GridView gridView;
+    private ProgressBar progressBar;
+    private TextView txtvError;
+    private Button butRetry;
+    private TextView txtvEmpty;
+
     public static final String TAG = "HomePageFragment";
+    private List<ItunesAdapter.Podcast> searchResults;
+    private List<ItunesAdapter.Podcast> topList;
+    private Subscription subscription;
+
+    private static final String API_URL = "https://itunes.apple.com/search?media=podcast&term=%s";
 
     public HomePageFragment(){
 
@@ -34,6 +77,131 @@ public class HomePageFragment extends Fragment{
        View root = inflater.inflate(R.layout.home_page, container, false);
 
        return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.itunes_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView sv = (SearchView) MenuItemCompat.getActionView(searchItem);
+        MenuItemUtils.adjustTextColor(getActivity(), sv);
+        sv.setQueryHint(getString(R.string.search_itunes_label));
+        sv.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                sv.clearFocus();
+                search(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                if(searchResults != null) {
+                    searchResults = null;
+                    updateData(topList);
+                }
+                return true;
+            }
+        });
+    }
+
+    private void search(String query) {
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
+        gridView.setVisibility(View.GONE);
+        txtvError.setVisibility(View.GONE);
+        butRetry.setVisibility(View.GONE);
+        txtvEmpty.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        subscription = rx.Observable.create((Observable.OnSubscribe<List<ItunesAdapter.Podcast>>) subscriber -> {
+            String encodedQuery = null;
+            try {
+                encodedQuery = URLEncoder.encode(query, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                // this won't ever be thrown
+            }
+            if (encodedQuery == null) {
+                encodedQuery = query; // failsafe
+            }
+
+            //Spaces in the query need to be replaced with '+' character.
+            String formattedUrl = String.format(API_URL, query).replace(' ', '+');
+
+            OkHttpClient client = AntennapodHttpClient.getHttpClient();
+            Request.Builder httpReq = new Request.Builder()
+                    .url(formattedUrl)
+                    .header("User-Agent", ClientConfig.USER_AGENT);
+            List<ItunesAdapter.Podcast> podcasts = new ArrayList<>();
+            try {
+                Response response = client.newCall(httpReq.build()).execute();
+
+                if(response.isSuccessful()) {
+                    String resultString = response.body().string();
+                    JSONObject result = new JSONObject(resultString);
+                    JSONArray j = result.getJSONArray("results");
+
+                    for (int i = 0; i < j.length(); i++) {
+                        JSONObject podcastJson = j.getJSONObject(i);
+                        ItunesAdapter.Podcast podcast = ItunesAdapter.Podcast.fromSearch(podcastJson);
+                        podcasts.add(podcast);
+                    }
+                }
+                else {
+                    String prefix = getString(R.string.error_msg_prefix);
+                    subscriber.onError(new IOException(prefix + response));
+                }
+            } catch (IOException | JSONException e) {
+                subscriber.onError(e);
+            }
+            subscriber.onNext(podcasts);
+            subscriber.onCompleted();
+        })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(podcasts -> {
+                    progressBar.setVisibility(View.GONE);
+                    updateData(podcasts);
+                }, error -> {
+                    Log.e(TAG, Log.getStackTraceString(error));
+                    progressBar.setVisibility(View.GONE);
+                    txtvError.setText(error.toString());
+                    txtvError.setVisibility(View.VISIBLE);
+                    butRetry.setOnClickListener(v -> search(query));
+                    butRetry.setVisibility(View.VISIBLE);
+                });
+    }
+
+    /**
+     * Replace adapter data with provided search results from SearchTask.
+     * @param result List of Podcast objects containing search results
+     */
+    void updateData(List<ItunesAdapter.Podcast> result) {
+        this.searchResults = result;
+        adapter.clear();
+        if (result != null && result.size() > 0) {
+            gridView.setVisibility(View.VISIBLE);
+            txtvEmpty.setVisibility(View.GONE);
+            for (ItunesAdapter.Podcast p : result) {
+                adapter.add(p);
+            }
+            adapter.notifyDataSetInvalidated();
+        } else {
+            gridView.setVisibility(View.GONE);
+            txtvEmpty.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
