ad83eae15aaf876ae5b25ef90c4fb33bf947e3d3
davidlegare <davidlegare01@gmail.com>
2018-03-09 19:35:18 -0500

    Changed test from androidTest to Test #5

--- app/src/test/java/dummy/ToplistFragmentTest.java
+++ app/src/test/java/dummy/ToplistFragmentTest.java
@@ -1,8 +1,44 @@
+package dummy;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by David on 2018-03-09.
- */
-public class ToplistFragmentTest {
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
+import de.danoeh.antennapod.activity.TestActivity;
+import de.danoeh.antennapod.fragment.ToplistFragment;
+
+/**
+ * Created by David on 2018-03-07.
+ */
+public class ToplistFragmentTest{
+
+    @Mock
+    private TestActivity mockActivity;
+
+    @Mock
+    private ToplistFragment mockFragment;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockActivity = Mockito.mock(TestActivity.class);
+        mockFragment = Mockito.mock(ToplistFragment.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mockActivity = null;
+        mockFragment = null;
+    }
+
+    @Test
+    public void testToplist(){
+        mockFragment.loadToplist();
+        assertTrue(true);
+    }
 }
