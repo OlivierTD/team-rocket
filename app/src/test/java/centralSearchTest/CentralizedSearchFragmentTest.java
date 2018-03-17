6d8cc72ab377bf7302db8d91a046b7e7d4e495e1
davidlegare <davidlegare01@gmail.com>
2018-03-16 20:47:10 -0400

    Discarded changes made from master #72

--- app/src/test/java/centralSearchTest/CentralizedSearchFragmentTest.java
+++ app/src/test/java/centralSearchTest/CentralizedSearchFragmentTest.java
@@ -1,28 +1,22 @@
 package centralSearchTest;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
-import de.danoeh.antennapod.adapter.itunes.ItunesAdapter;
 import de.danoeh.antennapod.fragment.CentralizedSearchFragment;
-import de.danoeh.antennapod.fragment.ToplistFragment;
-import de.mfietz.fyydlin.FyydClient;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 /**
  * Created by David on 2018-03-15.
  */
 public class CentralizedSearchFragmentTest {
 
-    
+    CentralizedSearchFragment searchFragment;
 
+    @Test
+    public void search() throws Exception {
+        /*
+        TO DO
+        */
     }
 }
