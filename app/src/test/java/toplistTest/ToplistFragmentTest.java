70e52976e9022e57c761302f14b64e17beb05fcd
davidlegare <davidlegare01@gmail.com>
2018-03-20 13:18:49 -0400

    Reverted unit test changes to what is in master
    Reverted work made on the unit test to reflect what is currently on master. Will complete new unit test shortly.

--- app/src/test/java/toplistTest/ToplistFragmentTest.java
+++ app/src/test/java/toplistTest/ToplistFragmentTest.java
@@ -1,59 +1,22 @@
-package toplistTest;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-
-import static org.mockito.Mockito.doAnswer;
+package centralSearchTest;
 
 import org.junit.Test;
-import org.junit.After;
-import org.junit.Before;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
-import java.util.Arrays;
+import de.danoeh.antennapod.fragment.CentralizedSearchFragment;
 
-import de.danoeh.antennapod.fragment.ToplistFragment;
+import static org.junit.Assert.*;
 
 /**
- * Created by David on 2018-03-07.
+ * Created by David on 2018-03-15.
  */
+public class CentralizedSearchFragmentTest {
 
-public class ToplistFragmentTest{
-
-    //Mock values for onCreateView
-    @Mock
-    LayoutInflater mockInflater;
-    @Mock
-    ViewGroup mockViewGroup;
-    @Mock
-    Bundle mockBundle;
-
-    private ToplistFragment testFragment;
-
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        testFragment = Mockito.mock(ToplistFragment.class);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        testFragment = null;
-    }
+    CentralizedSearchFragment searchFragment;
 
     @Test
-    public void testToplist(){
-        doAnswer(new Answer<Void>() {
-            public Void answer(InvocationOnMock invocation) {
-                Object[] args = invocation.getArguments();
-                System.out.println("called with arguments: " + Arrays.toString(args));
-                return null;
-            }
-        }).when(testFragment).onCreateView(mockInflater, mockViewGroup, mockBundle);
+    public void search() throws Exception {
+        /*
+        TO DO
+        */
     }
 }
