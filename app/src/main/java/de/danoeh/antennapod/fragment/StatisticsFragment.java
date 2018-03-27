1d096765252d8b7a2377ee38b07a293d63bdf4d8
Vartan Benohanian <vartanbeno@gmail.com>
2018-03-26 23:57:11 -0400

    #92 add stats fragment, doesn't display anything useful yet

---
+++ app/src/main/java/de/danoeh/antennapod/fragment/StatisticsFragment.java
@@ -0,0 +1,48 @@
+package de.danoeh.antennapod.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.danoeh.antennapod.R;
+
+/**
+ * Created by Vartan on 2018-03-26.
+ */
+
+public class StatisticsFragment extends Fragment{
+
+    public static final String TAG = "StatisticsFragment";
+
+    public StatisticsFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View root = inflater.inflate(R.layout.statistics_page, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+    }
+}
