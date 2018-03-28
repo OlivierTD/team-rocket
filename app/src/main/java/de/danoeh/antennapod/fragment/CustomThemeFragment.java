16466594ae6696801ea925cb2bc36a6038b6ed3f
Anania-y <anania.y@hotmail.com>
2018-03-28 00:40:00 -0400

    Added HSV Color Picker to dialog classes
    Not my creation, got it from:
    
    https://github.com/jesperborgstrup/buzzingandroid/blob/master/src/com/buzzingandroid/ui/HSVColorPickerDialog.java
    
    #50 #89 #90

--- app/src/main/java/de/danoeh/antennapod/fragment/CustomThemeFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/CustomThemeFragment.java
@@ -4,21 +4,14 @@ package de.danoeh.antennapod.fragment;
  * Created by Anania on 3/26/2018.
  */
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import de.danoeh.antennapod.R;
-import de.danoeh.antennapod.activity.MainActivity;
+import de.danoeh.antennapod.dialog.HSVColorPickerDialog;
 
 public class CustomThemeFragment extends Fragment {
 
@@ -39,6 +32,16 @@ public class CustomThemeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View root = inflater.inflate(R.layout.custom_theme_fragment, container, false);
+
+        HSVColorPickerDialog cpd = new HSVColorPickerDialog( getActivity(), 0xFF4488CC, new HSVColorPickerDialog.OnColorSelectedListener() {
+            @Override
+            public void colorSelected(Integer color) {
+                // Do something with the selected color
+            }
+        });
+        cpd.setTitle( "Pick a color" );
+        //cpd.setNoColorButton( R.string.no_color );
+        cpd.show();
 
         return root;
     }
