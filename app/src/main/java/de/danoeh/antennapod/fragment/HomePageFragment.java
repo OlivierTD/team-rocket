c68011fd1e07279e83480b55d67b2b173361331b
Batoul Yehia <batoul.yehia@gmail.com>
2018-03-29 18:31:43 -0400

    Created a redirect from the homepage fragment to the centralized search fragment, in a new page #8

--- app/src/main/java/de/danoeh/antennapod/fragment/HomePageFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/HomePageFragment.java
@@ -1,6 +1,11 @@
 package de.danoeh.antennapod.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,6 +40,36 @@ public class HomePageFragment extends Fragment{
         return root;
     }
 
+
+    //creates search menu on top
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.home_toolbar, menu);
+    }
+
+    //
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_home:
+                Log.d("Home search", "Redirecting to new page to centralized search fragment...");
+                reDirect();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //method that allows a redirect from the homepage to the centralized search page
+    public void reDirect(){
+        CentralizedSearchFragment centralizedSearchFragment = new CentralizedSearchFragment();
+        FragmentManager manager = getFragmentManager();
+        manager.beginTransaction().replace(R.id.main_view,centralizedSearchFragment,centralizedSearchFragment.getTag())
+                .commit();
+
+    }
     @Override
     public void onStart(){
         super.onStart();
