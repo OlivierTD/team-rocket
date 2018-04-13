5d96e85217435f1e0dfdc2935802d9c5074890dc
Anania-y <anania.y@hotmail.com>
2018-04-13 00:15:31 -0400

    Refcatored Code based on CodeClimate recomendation
    #50 #89 #90

--- app/src/main/java/de/danoeh/antennapod/fragment/CustomThemeFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/CustomThemeFragment.java
@@ -22,8 +22,12 @@ import de.danoeh.antennapod.menuhandler.NavDrawerActivity;
 public class CustomThemeFragment extends Fragment implements View.OnClickListener {
 
     public static final String TAG = "CustomThemeFragment";
-    private Button firstColor, secondColor, thirdColor, setColors;
-    private Integer color1, color2, color3;
+    //private Button firstColor, secondColor, thirdColor, setColors;
+    //private Integer color1, color2, color3;
+    private Button[] colorButtons;
+    private Integer[] colors;
+    private Button setColors;
+    private static int NUMBER_OF_COLORS = 3;
 
     //Mandatory Constructor
     public CustomThemeFragment() {
@@ -44,110 +48,86 @@ public class CustomThemeFragment extends Fragment implements View.OnClickListene
         //cpd.setNoColorButton( R.string.no_color );
 
         View v = inflater.inflate(R.layout.custom_theme_fragment, container, false);
-        firstColor = (Button) v.findViewById(R.id.custom_theme_color_1);
-        secondColor = (Button) v.findViewById(R.id.custom_theme_color_2);
-        thirdColor = (Button) v.findViewById(R.id.custom_theme_color_3);
+        Button firstColor = (Button) v.findViewById(R.id.custom_theme_color_1);
+        Button secondColor = (Button) v.findViewById(R.id.custom_theme_color_2);
+        Button thirdColor = (Button) v.findViewById(R.id.custom_theme_color_3);
+        colorButtons = new Button[]{firstColor, secondColor, thirdColor};
+        colors = new Integer[NUMBER_OF_COLORS];
+
         setColors = (Button) v.findViewById(R.id.custom_theme_set);
 
-        firstColor.setOnClickListener(this);
-        secondColor.setOnClickListener(this);
-        thirdColor.setOnClickListener(this);
+        for(int i = 0; i < NUMBER_OF_COLORS; i++){
+            colorButtons[i].setOnClickListener(this);
+        }
         setColors.setOnClickListener(this);
-
-
 
         return v;
     }
 
+    private void getColorFromUser(int i){
+        HSVColorPickerDialog cpd1 = new HSVColorPickerDialog(getActivity(), 0xFF4488CC, new HSVColorPickerDialog.OnColorSelectedListener() {
+            @Override
+            public void colorSelected(Integer color) {
+                //if a color was selected
+                if(color != null){
+                    // Do something with the selected color
+                    colorButtons[i].setBackgroundColor(color);
+                    colors[i] = color;
+                }
+            }
+        });
+        cpd1.setTitle("Pick your color " + (i+1) + "!");
+        cpd1.show();
+    }
 
+    private void setThemeColor(Integer i) {
+        if(colors[i] == null){
+            return;
+        }
+
+        switch(i){
+            case 0:
+                //background
+                ((MainActivity) getActivity()).setActivityBackgroundColor(colors[i]);
+                break;
+            case 1:
+                //nav bar
+                View navList = ((MainActivity) getActivity()).findViewById(R.id.nav_list);
+                navList.setBackgroundColor(colors[i]);
+                break;
+            case 2:
+                //nav layout
+                View navTitle = ((MainActivity) getActivity()).findViewById(R.id.nav_layout);
+                navTitle.setBackgroundColor(colors[i]);
+                //nav settings
+                View navSettings = ((MainActivity) getActivity()).findViewById(R.id.nav_settings);
+                navSettings.setBackgroundColor(colors[i]);
+                //action bar
+                //Code would work in API 11
+                //ColorDrawable colorDrawable = new ColorDrawable(color3));
+                //((MainActivity) getActivity()).getActionBar().setBackgroundDrawable(colorDrawable);
+                break;
+        }
+    }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.custom_theme_color_1:
-                HSVColorPickerDialog cpd1 = new HSVColorPickerDialog(getActivity(), 0xFF4488CC, new HSVColorPickerDialog.OnColorSelectedListener() {
-                    @Override
-                    public void colorSelected(Integer color) {
-                        //if a color was selected
-                        if(color != null){
-                            // Do something with the selected color
-                            firstColor.setBackgroundColor(color);
-                            color1 = color;
-                        }
-                    }
-                });
-                cpd1.setTitle("Pick your first color!");
-                cpd1.show();
-
-
+                getColorFromUser(0);
                 break;
             case R.id.custom_theme_color_2:
-                HSVColorPickerDialog cpd2 = new HSVColorPickerDialog(getActivity(), 0xFF4488CC, new HSVColorPickerDialog.OnColorSelectedListener() {
-                    @Override
-                    public void colorSelected(Integer color) {
-                        //if a color was selected
-                        if(color != null){
-                            // Do something with the selected color
-                            secondColor.setBackgroundColor(color);
-                            color2 = color;
-                        }
-                    }
-                });
-                cpd2.setTitle("Pick your second color!");
-                cpd2.show();
-
-
+                getColorFromUser(1);
                 break;
             case R.id.custom_theme_color_3:
-                HSVColorPickerDialog cpd3 = new HSVColorPickerDialog(getActivity(), 0xFF4488CC, new HSVColorPickerDialog.OnColorSelectedListener() {
-                    @Override
-                    public void colorSelected(Integer color) {
-                        //if a color was selected
-                        if(color != null){
-                            // Do something with the selected color
-                            thirdColor.setBackgroundColor(color);
-                            color3 = color;
-                        }
-                    }
-                });
-                cpd3.setTitle("Pick your third color!");
-                cpd3.show();
-
-
+                getColorFromUser(2);
                 break;
             case R.id.custom_theme_set:
-
-                if(color1 != null ) {
-                    //background
-                    ((MainActivity) getActivity()).setActivityBackgroundColor(color1);
-
-
+                for(int i = 0; i < NUMBER_OF_COLORS; i++){
+                    setThemeColor(i);
                 }
-                if(color2 != null ) {
-                    //nav bar
-                    View navList = ((MainActivity) getActivity()).findViewById(R.id.nav_list);
-                    navList.setBackgroundColor(color2);
-
-                }
-                if(color3 != null ) {
-
-                    //nav layout
-                    View navTitle = ((MainActivity) getActivity()).findViewById(R.id.nav_layout);
-                    navTitle.setBackgroundColor(color3);
-                    //nav settings
-                    View navSettings = ((MainActivity) getActivity()).findViewById(R.id.nav_settings);
-                    navSettings.setBackgroundColor(color3);
-                    //action bar
-                    //Code would work in API 11
-                    //ColorDrawable colorDrawable = new ColorDrawable(color3));
-                    //((MainActivity) getActivity()).getActionBar().setBackgroundDrawable(colorDrawable);
-
-
-
-                }
-
-
-            }
+                break;
         }
+    }
 
 
 
