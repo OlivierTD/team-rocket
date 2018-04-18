87b53309b1f65d277c9253988b4c7d71e54b5a4b
Vartan Benohanian <vartanbeno@gmail.com>
2018-04-18 01:48:14 -0400

    add unit test for resetting stats #176

---
+++ app/src/test/java/resetStatsTest/ResetStatsTest.java
@@ -0,0 +1,35 @@
+package resetStatsTest;
+
+import android.view.View;
+import android.widget.Button;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import de.danoeh.antennapod.fragment.StatisticsFragment;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by vartanbeno on 2018-04-18.
+ */
+
+public class ResetStatsTest {
+
+    @Test
+    public void testResetStatsButton() {
+
+        StatisticsFragment statisticsFragment = new StatisticsFragment();
+        Button mockButton = Mockito.mock(Button.class);
+
+        statisticsFragment.setResetStatsButton(mockButton);
+        statisticsFragment.setResetStatsFunctionality();
+
+        // test that an arbitrary OnClickListener object is set on the button
+        verify(mockButton).setOnClickListener((View.OnClickListener) any());
+
+
+    }
+
+}
